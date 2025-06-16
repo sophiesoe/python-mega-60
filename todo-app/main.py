b518bfbd9e37a8404bfb3d7e6b00c0cc9339f021
sophiesoe <devsophiesoe@gmail.com>
2025-06-17 00:19:06 +0630

    add: todo feats

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,40 +1,63 @@
-todos = ["clean"]
 
-def print_todos():
+def print_todos(todos):
   if len(todos) == 0:
      print("No todos added.")
   for index, todo in enumerate(todos, start=0):
-        print(f"{index + 1}. {todo.title()}")
+        print(f"{index + 1}. {todo.title().strip("\n")}")
+
+def readFile():
+  with open("todos.txt", "r") as file:
+    todos = file.readlines()
+    return todos
+
+def writeTodo(todos):
+  with open("todos.txt", "w") as file:
+    todos = file.writelines(todos)
+
+
+def addTodo(todo):
+  todos = readFile()
+  todos.append(todo)
+  writeTodo(todos)
+   
 
 
 while True:
   user_action = input("Type add, show, edit, complete or exit\n").strip()
   match user_action:
     case "add":
-      todo = input("Enter todo: ")
-      todos.append(todo)
+      todo = input("Enter todo: ") + "\n"
+      addTodo(todo)
     case "show":
-      print_todos()
+      todos = readFile()
+      print_todos(todos)
     case "edit":
-      print_todos()
+      todos = readFile()
+      print_todos(todos)
+
       if len(todos) > 0:     
         todo_item = input("Number of todo to edit: ")
         todo_index = int(todo_item) -1
         if(todo_index < len(todos)):
-          editTodo = input(f"Todo: {todos[todo_index].title()}\nType edit todo: ")
+          editTodo = input(f"Todo: {todos[todo_index].title()}\nType edit todo: ") + "\n"
           todos[todo_index] = editTodo
-          print_todos()
+          writeTodo(todos)
+          todos = readFile()
+          print_todos(todos)
         else: print("No todo found with this number.")
       else: print("Empty todos")
     case "complete":
-        print_todos()
+        todos = readFile()
+        print_todos(todos)
         todo_item = input("Number of todo to complete: ")
         todo_index = int(todo_item) -1
         if(todo_index < len(todos)):
           todos.pop(todo_index)
           if len(todos) == 0:
               print("You have finished all todos!")
-          else: print_todos()
+          else: 
+            todos = readFile()
+            print_todos(todos)
         else: print("No todo found with this number.")
     case "exit":
       break
