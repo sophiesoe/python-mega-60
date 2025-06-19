3f9ad3fab50ee35534189f61056de875e2f76598
sophiesoe <devsophiesoe@gmail.com>
2025-06-20 00:16:25 +0630

    chore: edit feat

--- todo-app/main.py
+++ todo-app/main.py
@@ -24,42 +24,44 @@ def addTodo(todo):
 
 while True:
   user_action = input("Type add, show, edit, complete or exit\n").strip()
-  match user_action:
-    case "add":
-      todo = input("Enter todo: ") + "\n"
-      addTodo(todo)
-    case "show":
-      todos = readFile()
-      print_todos(todos)
-    case "edit":
+  if user_action.startswith("add"):
+    todo = user_action[4:]
+    addTodo(todo)
+  if user_action.startswith("show"):
+    todos = readFile()
+    print_todos(todos)
+  if user_action.startswith("edit"):
       todos = readFile()
       print_todos(todos)
 
-      if len(todos) > 0:     
-        todo_item = input("Number of todo to edit: ")
-        todo_index = int(todo_item) -1
-        if(todo_index < len(todos)):
-          editTodo = input(f"Todo: {todos[todo_index].title()}\nType edit todo: ") + "\n"
-          todos[todo_index] = editTodo
-          writeTodo(todos)
-          todos = readFile()
-          print_todos(todos)
-        else: print("No todo found with this number.")
-      else: print("Empty todos")
-    case "complete":
-        todos = readFile()
-        print_todos(todos)
-        todo_item = input("Number of todo to complete: ")
-        todo_index = int(todo_item) -1
-        if(todo_index < len(todos)):
-          todos.pop(todo_index)
-          if len(todos) == 0:
-              print("You have finished all todos!")
-          else: 
+      if len(todos) > 0:
+        try:     
+          todo_item = user_action[4:]
+          todo_index = int(todo_item) -1
+          if(todo_index < len(todos)):
+            editTodo = input(f"Todo: {todos[todo_index].title()}\nType edit todo: ") + "\n"
+            todos[todo_index] = editTodo
+            writeTodo(todos)
             todos = readFile()
             print_todos(todos)
+        except ValueError:
+          print("Please type number after edit.")
         else: print("No todo found with this number.")
-    case "exit":
-      break
+      else: print("Empty todos")
+  if user_action.startswith("complete"):
+      todos = readFile()
+      print_todos(todos)
+      todo_item = user_action[8:]
+      todo_index = int(todo_item) -1
+      if(todo_index < len(todos)):
+        todos.pop(todo_index)
+        if len(todos) == 0:
+            print("You have finished all todos!")
+        else: 
+          todos = readFile()
+          print_todos(todos)
+      else: print("No todo found with this number.")
+  if user_action.startswith("exit"):
+    break
     
 print("Bye!")
